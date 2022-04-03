5cecf558ba5aa9e353891db96dbeb91503bbf577
Fatih <fatih.simsek@akbank.com>
2022-04-03 15:42:23 +0300

    JpaRepository migrate

--- src/main/java/com/trendyol/linkconverter/repository/TransactionHistoryRepository.java
+++ src/main/java/com/trendyol/linkconverter/repository/TransactionHistoryRepository.java
@@ -1,26 +1,10 @@
 package com.trendyol.linkconverter.repository;
 
 import com.trendyol.linkconverter.entity.TransactionHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.UUID;
 
 @Repository
-public class TransactionHistoryRepository {
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    public void save(TransactionHistory transactionHistory) {
-        UUID id = transactionHistory.getId();
-
-        if(id == null) {
-            entityManager.persist(transactionHistory);
-        }
-        else {
-            entityManager.merge(transactionHistory);
-        }
-    }
+public interface TransactionHistoryRepository extends JpaRepository<TransactionHistory, UUID> {
 }
